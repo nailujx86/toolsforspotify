bba774d1450a408dac98be2a779e3775dfdee358
Julian B <julian.blazek@outlook.de>
2020-05-04 20:34:34 +0200

    Added csv export

--- routes/analyze.js
+++ routes/analyze.js
@@ -35,6 +35,14 @@ router.get('/json/:playlist', requiresLogin, (req, res, next) => {
     return res.json(res.trackInfoData);
 });
 
+router.get('/csv/:playlist', requiresLogin, (req, res, next) => {
+    res.set('Content-Type', 'text/csv')
+    res.set('Content-Disposition', 'attachment;filename="' + res.playlistData.name + '-Analysis.csv"')
+    var output = "id;danceability;energy;key;loudness;mode;speechiness;acousticness;instrumentalness;liveness;valence;tempo;duration_ms;time_signature\n";
+    output += res.trackInfoData.map(t => t.id + ";" + t.danceability + ";" + t.energy + ";" + t.key + ";" + t.loudness + ";" + t.mode + ";" + t.speechiness + ";" + t.acousticness + ";" + t.instrumentalness + ";" + t.liveness + ";" + t.valence + ";" + t.tempo + ";" + t.duration_ms + ";" + t.time_signature).join("\n");
+    return res.end(output);
+});
+
 router.get('/charts/:playlist', requiresLogin, (req, res, next) => {
     res.locals.meta.title = "\"" + res.playlistData.name + "\" Analysis";
     res.locals.data = JSON.stringify(res.trackInfoData);
