8339ed589481a56e9646fd2cfbff3d26ec5b5395
Julian B <julian.blazek@outlook.de>
2020-05-07 20:55:47 +0200

    Updated formatting

--- middleware/playlistAnalyzer.js
+++ middleware/playlistAnalyzer.js
@@ -6,32 +6,32 @@ var router = express.Router();
 router.use(require('../middleware/playlistLoader'));
 
 router.use((req, res, next) => {
-    if(res.trackInfoData)
-        return next();
+  if (res.trackInfoData)
+    return next();
 
-    var spotifyUserApi = new SpotifyWebApi({
-        accessToken: req.cookies['access_token']
-    });
-    var ids = [];
-    ids = res.playlistData.tracks.map(t => t.track.uri.startsWith("spotify:track:") ? t.track.uri.replace("spotify:track:", "") : null).filter(t => t != null);
-    var count = Math.ceil(ids.length / 100);
-    var total = 0;
-    var trackInfos = [];
-    [...Array(count)].reduce((p, _) =>
-        p.then(_ =>
-            spotifyUserApi.getAudioFeaturesForTracks(ids.slice(total, total + 100)).then(data => {
-                total += 100;
-                trackInfos.push(...data.body.audio_features);
-            })
-        )
-        , Promise.resolve())
-        .then(() => {
-            res.trackInfoData = trackInfos;
-            return next();
-        })
-        .catch((err) => {
-            return next(err);
-        })
+  var spotifyUserApi = new SpotifyWebApi({
+    accessToken: req.cookies['access_token']
+  });
+  var ids = [];
+  ids = res.playlistData.tracks.map(t => t.track.uri.startsWith("spotify:track:") ? t.track.uri.replace("spotify:track:", "") : null).filter(t => t != null);
+  var count = Math.ceil(ids.length / 100);
+  var total = 0;
+  var trackInfos = [];
+  [...Array(count)].reduce((p, _) =>
+    p.then(_ =>
+      spotifyUserApi.getAudioFeaturesForTracks(ids.slice(total, total + 100)).then(data => {
+        total += 100;
+        trackInfos.push(...data.body.audio_features);
+      })
+    )
+    , Promise.resolve())
+    .then(() => {
+      res.trackInfoData = trackInfos;
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    })
 });
 
 module.exports = router;
